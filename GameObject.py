d4b6c8114684bc4098c3b1742a64f1639b5f0cfe
BEATRIZ DAMASCENO FERNANDES <93746097+beatrizdamascenof@users.noreply.github.com>
2022-04-10 14:30:45 -0400

    Update GameObject.py
    definição dos sprites para bomb e flag

--- GameObject.py
+++ GameObject.py
@@ -5,5 +5,7 @@ class GameObject(pygame.sprite.Sprite):
 
     def __init__(self, sub):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('image/bomb.jpg')
-        self.image = pygame.transform.scale(self.image, (sub, sub))
+        self.image1 = pygame.image.load('image/bomb.jpg')
+        self.image1 = pygame.transform.scale(self.image1, (sub, sub))
+        self.image2 = pygame.image.load('image/flag.jpg')
+        self.image2 = pygame.transform.scale(self.image2, (sub, sub))
