1e33e19644921dac6d3245548197c1be622bab1b
Cibele Arahel Menezes Batista <99154448+cibelearahel@users.noreply.github.com>
2022-04-11 11:20:08 -0400

    font update

--- Config.py
+++ Config.py
@@ -4,14 +4,17 @@ pygame.font.init()
 
 size = (600, 600)
 
-BLUE = (0, 0, 139)
+MINT = (193, 255, 196)
+MINT_GREEN = (36, 183, 119)
 WHITE = (220, 220, 220)
 RED = (255, 0, 0)
 ORANGE = (247, 131, 7)
 PINK = (255, 0, 255)
 GREEN = (50, 205, 50)
+DARK_GREEN = (10, 114, 17)
 YELLOW = (253, 208, 23)
 BLACK = (0, 0, 0)
+
 
 qtd_square = 8
 space_square = 2
@@ -19,10 +22,10 @@ margin = space_square // 2
 side_square = round((size[0] - (space_square * qtd_square + margin)) / qtd_square)
 parameter = [margin, margin, side_square, side_square]
 
-font = pygame.font.SysFont('Time New Roman', side_square)
-font_lost = pygame.font.SysFont('Tahoma', size[0] // 10)
-font_win = pygame.font.SysFont('Tahoma', size[0] // 8)
-font_flag = pygame.font.SysFont('Times New Roman', size[0] // 20)
+
+font = pygame.font.Font('fonts/Square.ttf', side_square)
+font_standard = pygame.font.Font('fonts/Square.ttf', size[0] // 10)
+
 
 qtd_mines = int(qtd_square * qtd_square * 0.15)
 with_flag = {}
